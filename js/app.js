672368d5c7826d32c2090304f43c0b4cafa0848d
Prosen Biswas <webprosen@gmail.com>
2023-03-03 14:28:21 +0600

    api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Load all ai tools
 const loadAiTools = async(dataLimit) => {
     // Fetch data from api
     const url = 'https://openapi.programming-hero.com/api/ai/tools';
@@ -6,8 +7,8 @@ const loadAiTools = async(dataLimit) => {
     displayAiTools(data.data.tools, dataLimit);
 }
 
+//  Display all ai tools
 const displayAiTools = (tools, dataLimit) => {
-
     // Limit & conditions check
     const seeMore = document.getElementById('see-more');
     if(dataLimit && tools.length > dataLimit){
@@ -36,7 +37,7 @@ const displayAiTools = (tools, dataLimit) => {
                         <span class="">${tool.published_in}</span>
                     </div>
                 </div>
-                <div class="tool-button d-flex align-items-center justify-content-center">
+                <div id="tool-button" onclick="loadToolDetails('${tool.id}')" class="d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#toolModal">
                     <i class="fa-solid fa-arrow-right"></i>
                 </div>
             </div>
@@ -49,7 +50,7 @@ const displayAiTools = (tools, dataLimit) => {
 //  By default calling 6 items
 loadAiTools(6);
 
-// See more display all data
+// See more to display all data
 document.getElementById('see-more').addEventListener('click', () => {
     loadingSpinner(true);
     loadAiTools();
@@ -63,4 +64,83 @@ const loadingSpinner = isLoading => {
     } else {
         loader.classList.add('d-none');
     }
+}
+
+// Load single item tool
+const loadToolDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayToolDetails(data.data)
+}
+
+const displayToolDetails = tool => {
+    // Modal description
+    const toolDescription = document.getElementById('tool-description');
+    toolDescription.innerText = tool.description;
+
+    // Modal Pricing
+    const planPrice1 = document.getElementById('plan-price-1');
+    planPrice1.innerText = tool.pricing? tool.pricing[0].price : 'Free of Cost';
+    const planName1 = document.getElementById('plan-name-1');
+    planName1.innerText = tool.pricing? tool.pricing[0].plan : 'Basic';
+
+    const planPrice2 = document.getElementById('plan-price-2');
+    planPrice2.innerText = tool.pricing? tool.pricing[1].price : 'Free of Cost';
+    const planName2 = document.getElementById('plan-name-2');
+    planName2.innerText = tool.pricing? tool.pricing[1].plan : 'Pro';
+
+    const planPrice3 = document.getElementById('plan-price-3');
+    planPrice3.innerText = tool.pricing? tool.pricing[2].price : 'Free of Cost';
+    const planName3 = document.getElementById('plan-name-3');
+    planName3.innerText = tool.pricing? tool.pricing[2].plan : 'Enterprise';
+
+
+    // Modal features data
+    const modalFeatures = document.getElementById('modal-features');
+    if(tool.features != null){
+        modalFeatures.innerHTML = '';
+        for(const feature of Object.values(tool.features)){
+            const li = document.createElement('li');
+            li.textContent = feature.feature_name;
+            modalFeatures.appendChild(li);
+        }
+    } else {
+        modalFeatures.innerHTML = 'No data found';
+        modalFeatures.classList.add('ps-0');
+    }
+
+    // Modal Integrations data
+    const modalIntegrations = document.getElementById('modal-integrations');
+    if(tool.integrations != null){
+        modalIntegrations.innerHTML = '';
+        for(const integration of tool.integrations){
+            const li = document.createElement('li');
+            li.textContent = integration;
+            modalIntegrations.appendChild(li);
+        }
+    } else {
+        modalIntegrations.innerHTML = 'No data found';
+        modalIntegrations.classList.add('ps-0');
+    }
+
+    // Modal image
+    const modalImage = document.getElementById('modal-image');
+    modalImage.setAttribute('src' , tool.image_link[0]);
+
+    // Image accuracy badge 
+    const accuracyBadge = document.getElementById('accuracy-badge');
+    const accuracyScore = document.getElementById('accuracy-score');
+    if(tool.accuracy.score != null){
+        accuracyBadge.classList.remove('d-none');
+        accuracyScore.innerText = tool.accuracy.score;
+    } else {
+        accuracyBadge.classList.add('d-none');
+    }
+    
+    // Question & answer
+    const ques = document.getElementById('question');
+    const ans = document.getElementById('answer');
+    ques.innerText = tool.input_output_examples? tool.input_output_examples[0].input : 'Can you give any example?';
+    ans.innerText = tool.input_output_examples? tool.input_output_examples[0].output : 'No! Not Yet! Take a break!!!';
 }
